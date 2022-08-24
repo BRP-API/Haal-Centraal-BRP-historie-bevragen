594a108c28dec2418ab284ef8ee8a99d34b105e0
Melvin Lee <melvin.lee@iswish.nl>
2022-08-24 09:16:21 +0200

    feat: setup automation voor postgresql

---
+++ features/step_definitions/world.js
@@ -0,0 +1,24 @@
+class World {
+    constructor(parameters) {
+        this.context = parameters;
+        this.context.dataPath = "src/BrpHistorieStub/Data";
+        this.context.serverUrl = "http://localhost:5000/haalcentraal/api/brphistorie";
+        this.context.persoon = undefined;
+        this.context.sql = {
+            useDb: true,
+            logStatements: true,
+            poolConfig: {
+                user: "root",
+                host: "localhost",
+                database: "rvig_haalcentraal_testdata",
+                password: "root",
+                port: 5432
+            }
+        };
+    }
+}
+
+// set this environment variable to bypass DEPTH_ZERO_SELF_SIGNED_CERT error when using self-signed certificates
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+module.exports = {World}
