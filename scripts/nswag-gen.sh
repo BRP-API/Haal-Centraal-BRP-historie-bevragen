e2c4e3a840aa863ab75e71d8a7b7b5c348be1036
Melvin Lee <melvin.lee@iswish.nl>
2024-10-30 08:07:17 +0100

    chore: feature map + bijbehorende scripts verwijderd

--- scripts/nswag-gen.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-nswag run src/BrpHistorieStub/Server.nswag /runtime:Net60
-nswag run src/BrpHistorieProxy/DataTransferObjects.nswag /runtime:Net60
-nswag run src/BrpHistorieProxy/GbaDataTransferObjects.nswag /runtime:Net60
